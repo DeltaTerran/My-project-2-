6fab925fb6ef8b698d14cfa789a3bc1758f4bef2
DeltaTerran <96633825+DeltaTerran@users.noreply.github.com>
2024-11-22 16:12:38 +0200

    Starting to Add ads

--- Assets/Scripts/SceneManagment/RestartLvl.cs
+++ Assets/Scripts/SceneManagment/RestartLvl.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
 
 public class RestartLvl : MonoBehaviour
 {
+    private InterstitialAd _interAd;
+    private const string _interstitialId = "ca-app-pub-1266056041937204~6113929937";
+
+    void RequestInterstitial()
+    {
+        _interAd = new InterstitialAd(_interstitialId);
+        AdRequest adRequest = new AdRequest.Builder().Build();
+        _interAd.LoadAd(adRequest);
+    }
+    public void ShowAd()
+    {
+        _interAd.Show();
+    }
+    public void QuitButton() 
+    {
+        Application.Quit(); 
+    }
+
+    public void Reborn()
+    {
+
+    }
     public void RestartLevel()
     {
         moveorb.ResetMVValues();
