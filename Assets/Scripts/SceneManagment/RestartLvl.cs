866c603b0a222b0bc8bcb55a17497478503f9a7c
DeltaTerran <96633825+DeltaTerran@users.noreply.github.com>
2024-11-27 16:58:25 +0200

    Added Textures

--- Assets/Scripts/SceneManagment/RestartLvl.cs
+++ Assets/Scripts/SceneManagment/RestartLvl.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using GoogleMobileAds.Api;
+using System;
 
 public class RestartLvl : MonoBehaviour
 {
     private InterstitialAd _interstitialAd;
     //private const string _interstitialId = "ca-app-pub-1266056041937204/7234272623";
-    private const string _adUnitId = "ca-app-pub-3940256099942544/1033173712";
+    private const string _adUnitId = "ca-app-pub-1266056041937204/7234272623";
     void Start()
     {
         MobileAds.Initialize(initStatus =>{});
         LoadInterstitialAd();
+        RegisterEventHandlers(_interstitialAd);
 
     }
+    
+
     //void RequestInterstitial()
     //{
     //    string adUnitId = "ca-app-pub-3940256099942544/1033173712"; // Тестовый ID
@@ -40,6 +44,18 @@ public class RestartLvl : MonoBehaviour
     //        interstitial.Show();
     //    }
     //}
+    void ClosesInterstitialAd()
+    {
+        if (_interstitialAd != null)
+        {
+            _interstitialAd.Destroy();
+            _interstitialAd = null;
+        }
+        else
+        {
+            Debug.Log("Баннер не существует или уже был закрыт.");
+        }
+    }
     public void LoadInterstitialAd()
     {
         if (_interstitialAd != null)
@@ -87,6 +103,44 @@ public class RestartLvl : MonoBehaviour
         GM.ResetGMValues();
         BlockSpawner.ResetBSValues();
         SceneManager.LoadScene("Game");
+    }
+    private void RegisterEventHandlers(InterstitialAd interstitialAd)
+    {
+        // Raised when the ad is estimated to have earned money.
+        interstitialAd.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(String.Format("Interstitial ad paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        interstitialAd.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Interstitial ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        interstitialAd.OnAdClicked += () =>
+        {
+            Debug.Log("Interstitial ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        interstitialAd.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Interstitial ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        interstitialAd.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Interstitial ad full screen content closed.");
+            ClosesInterstitialAd();
+            
+        };
+        // Raised when the ad failed to open full screen content.
+        interstitialAd.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Interstitial ad failed to open full screen content " +
+                           "with error : " + error);
+        };
     }
     #region Buttons
     public void QuitButton()
